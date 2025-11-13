b20fcfb155aad6037ad9b20e3558e3a935996e65
Khalkot <72662630+khalkot@user.noreply.github.com>
2025-11-13 20:58:10 +0200

    first commit

---
+++ src/GeneralInfo.jsx
@@ -0,0 +1,9 @@
+export default function GeneralInfo(){
+    return (
+        <>
+        <input type="text" placeholder="First Name..."/>
+        <input type="text" placeholder="Last Name..."/>
+        <input type="email" placeholder="Email@domain.com"/>
+
+    </>)
+}
