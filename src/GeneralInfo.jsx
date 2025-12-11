acad73a747da296b9bce6936da123d7a9c44024e
Khalkot <72662630+khalkot@user.noreply.github.com>
2025-12-11 08:58:11 +0200

    remove unused components

--- src/GeneralInfo.jsx
+++
@@ -1,9 +0,0 @@
-export default function GeneralInfo(){
-    return (
-        <>
-        <input type="text" placeholder="First Name..."/>
-        <input type="text" placeholder="Last Name..."/>
-        <input type="email" placeholder="Email@domain.com"/>
-
-    </>)
-}
